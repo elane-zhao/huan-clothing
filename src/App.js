d2c69f0e0c24781b4b0d40d045d31cae66ebed6e
Xinyi Zhao <huanhuan@Xinyis-MacBook-Pro.local>
2021-07-09 00:47:12 -0400

    Break homepage into smaller components and style menu-item component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
