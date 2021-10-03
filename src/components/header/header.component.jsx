8d7b70ec9408c1d0b4e93e6efe7306c6e7eba376
Xinyi Zhao <huanhuan@Xinyis-MacBook-Pro.local>
2021-10-02 22:09:37 -0700

    Add sign-in component, form-input and custom-button components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,8 +12,8 @@ const Header = () => (
       <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className="option" to="/shop">
-        CONTACT
+      <Link className="option" to="/signin">
+        SIGN IN
       </Link>
     </div>
   </div>
