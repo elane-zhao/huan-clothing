6fbdd8242de5f6b45e683c90ab84cf0b4d060fc4
Xinyi Zhao <xinyizhao23@gmail.com>
2021-10-25 00:44:56 -0700

    add Add To Cart custom button to collection-item component

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,12 +4,13 @@ import './custom-button.styles.scss';
 const CustomButton = ({
   children,
   isGoogleSignIn,
+  inverted,
   ...otherProps
 }) => (
   <button
     className={`custom-button ${
       isGoogleSignIn ? 'google-sign-in' : ''
-    }`}
+    } ${inverted ? 'inverted' : ''}`}
     {...otherProps}
   >
     {children}
